ab520712bedfb90d339eed7f88a650661bb05373
Zakhar Borodayev <zborodayev@gmail.com>
2024-04-18 01:16:19 +0500

    add selectedText

--- src/components/email-editor/EmailEditor.jsx
+++ src/components/email-editor/EmailEditor.jsx
@@ -3,23 +3,46 @@ import { Bold } from 'lucide-react';
 import { Italic } from 'lucide-react';
 import { Underline } from 'lucide-react';
 
-import styles from './EmailEditor.module.scss'
+import styles from './EmailEditor.module.css'
+import { useRef, useState } from 'react';
 export const EmailEditor = () => {
+  const [text, setText] = useState(`Кто может стать обладателем международной стипендии «Болашак» Участником программы может стать любой гражданин Республики Казахстан, претендующий на: получение степени магистра, доктора философии (PhD), доктора по профилю, обучение в резидентуре и прохождение стажировок.`)
+  const textRef = useRef(null)
+
+  const getSelectoinText = () => {
+    if(!textRef.current) return
+    const cursorStart = textRef.current?.selectionStart
+    const cursorEnd = textRef.current?.selectionEnd
+
+    const selectedText = text.substring(cursorStart,cursorEnd)
+    if(!selectedText) return
+    console.log(selectedText)
+  }
+
   return (
     <div>
       <h1 className={styles.h1}>Email-editor</h1>
       <div className={styles.card}>
-        <div className={styles.editor}>
-          Кто может стать обладателем международной стипендии «Болашак» Участником программы может стать любой гражданин Республики Казахстан, претендующий на: получение степени магистра, доктора философии (PhD), доктора по профилю, обучение в резидентуре и прохождение стажировок.
-        </div>
+        <textarea 
+        spellCheck='false'
+        ref={textRef}
+        className={styles.editor}
+        onClick={getSelectoinText}
+        value={text}
+        onChange={e => setText(e.target.value)}
+        >
+          {text}
+        </textarea>
         <div className={styles.actoins}>
-          <button>Send now</button>
-          <div className={styles.tools}>
-          <button><Eraser size={18}/></button>
-          <button><Bold size={18}/></button>
-          <button><Italic size={18}/></button>
-          <button><Underline size={18}/></button>
+          <div>
+
+          <button><Eraser size={17}/></button>
+          <button><Bold size={17}/></button>
+          <button><Italic size={17}/></button>
+          <button><Underline size={17}/></button>
+
           </div>
+          <button>Send now</button>
         </div>
       </div>
     </div>
--- src/components/email-list/EmailList.jsx
+++ src/components/email-list/EmailList.jsx
@@ -1,8 +1,11 @@
+import styles from './EmailList.module.css'
 export const EmailList = () => {
   return (
-    <>
+    <div className={styles.div}>
       <h1>Email-Lists</h1>
-      <div></div>
-    </>
+      <div>
+        <textarea spellCheck='false' className={styles.Listeditor}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, maiores voluptate reiciendis laudantium nisi magnam dolores harum quia tenetur molestias, in atque optio aut, pariatur ratione deserunt architecto quos ducimus.</textarea>
+      </div>
+    </div>
   )
 }
